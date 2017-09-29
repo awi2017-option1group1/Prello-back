3fcb8b929e85b5c6862f4c7ea07fb7f530a3f729
Alexis Andrieu <porquepix@hotmail.fr>
2017-09-29 17:13:14 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import * as bodyParser from 'body-parser'
 
 const app = express()
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 5000)
 app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
