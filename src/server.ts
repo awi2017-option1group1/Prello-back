4faf2e977c088ef4c6e199894dd8352ee24f4f17
Porquepix <porquepix@hotmail.fr>
2017-10-03 05:43:04 +0200

    fix: DB Connection in production

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,26 @@
-import { createConnection } from 'typeorm'
 import * as express from 'express'
-import * as compression from 'compression' // compresses requests
+import * as compression from 'compression'
 import * as bodyParser from 'body-parser'
+import { createConnection } from 'typeorm'
 
-import { User } from './entities/user'
 import  { connectionOptions } from './connectionParams'
 
-const app = express()
 export const ENV = process.env.NODE_ENV || 'development'
-console.log(connectionOptions[ENV])
+
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+app.use(compression())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.end('Hello World!')
+})
+
 createConnection(connectionOptions[ENV]).then(connection => {
-
-    app.set('port', process.env.PORT || 3000)
-    app.use(compression())
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.get('/', (req, res) => {
-      res.end('Hello World!')
-    })
-
     app.listen(app.get('port'), () => {
       console.log(('App is running at http://localhost:%d in %s mode'), app.get('port'), app.get('env'))
       console.log('Press CTRL-C to stop\n')
     })
 }).catch(error => console.log(error))
-
-module.exports = app
