723b483b8a906253a88fb10e3e6dabc8ef22a4dc
Quentin Bouygues <QuentinBouygues@users.noreply.github.com>
2017-10-05 12:47:56 +0200

    pouet

--- src/bl/password.ts
+++ src/bl/password.ts
@@ -2,12 +2,7 @@ import * as bcrypt from 'bcrypt-nodejs'
 
 export class Password {
     static encrypt(password: string) {
-        try {
-            var salt = bcrypt.genSaltSync(10)
-            return bcrypt.hashSync(password, salt)
-        } catch (hashError) {
-            console.log('Error while hashing password : ', hashError)
-            return
-        }
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
     }
 }
