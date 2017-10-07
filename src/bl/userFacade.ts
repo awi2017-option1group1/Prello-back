adcbf5d6e0386812c1e2241e075426dea7027af8
Alexis Andrieu <porquepix@hotmail.fr>
2017-10-07 19:05:48 +0200

    Fix the connection between the back and the front (#12)
    * fix: Fix the connection between the back and the front
    
    * fix: userFacade crypt password

--- src/bl/userFacade.ts
+++ src/bl/userFacade.ts
@@ -1,11 +1,17 @@
 import { getEntityManager } from 'typeorm'
+
+import { UserNotFoundException } from './errors/UserNotFoundException'
 import { User } from '../entities/user'
 import { Password } from './password'
 
 export class UserFacade {
     static async authenticate(email: string, password: string): Promise<User>  {
-        const user = await getEntityManager().getRepository(User).findOne({email: email,
-            password: Password.encrypt(password)})
+        const user = await getEntityManager()
+                            .getRepository(User)
+                            .findOne({
+                                email: email,
+                                password: Password.encrypt(password)
+                            })
         if (user) {
             return user
         } else {
