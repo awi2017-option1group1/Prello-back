3b4e2d46925973823c3f176bfb9142e88d6b6e4b
charlenepizzoni <charlene.pizzoni@gmail.com>
2017-10-19 13:14:57 +0200

    Refactoring  NotFoundExceptions
    Merging XNotFoundException in a single file : NotFoundException

--- src/bl/errors/TaskListNotFoundException.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from './BaseError'
-
-export class TaskListNotFoundException extends BaseError {
-    constructor(message: string) {
-        super(message)
-    }
-}
--- src/bl/errors/UserNotFoundException.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from './BaseError'
-
-export class UserNotFoundException extends BaseError {
-    constructor(message: string) {
-        super(message)
-    }
-}
