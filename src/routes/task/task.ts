03b0fad3d8284ee35f2997653f0851834e28e040
MathieuRobilliard <mathieu.robilliard@gmail.com>
2017-10-23 18:33:15 +0200

    [fix] Server and Routes changed for Card, TaskList, Task

--- src/routes/task/task.ts
+++ src/routes/task/task.ts
@@ -4,6 +4,7 @@ import { TaskFacade } from '../../bl/taskFacade'
 
 export class Task {
 
+    /*
     static async getAllFromTaskListId(req: express.Request, res: express.Response) {
         try {
             const task = await TaskFacade.getAllFromTaskListId(req.params.taskList_id)
@@ -11,11 +12,12 @@ export class Task {
         } catch (e) {
             res.status(404).json({ message: e.message})
         }
-    }
+    } */
 
     static async getOneById(req: express.Request, res: express.Response) {
         try {
-            const task = await TaskFacade.getById(req.params.task_id)
+            const task = await TaskFacade.getById(req.params.id)
+            // req.params.id is the id of the Task
             res.status(200).json(task)
         } catch (e) {
             res.status(404).json({ message: e.message})
@@ -24,7 +26,8 @@ export class Task {
 
     static async delete(req: express.Request, res: express.Response) {
         try {
-            const deletionSuccess = await TaskFacade.delete(req.params.task_id)
+            const deletionSuccess = await TaskFacade.delete(req.params.id)
+            // req.params.id is the id of the Task
             if (deletionSuccess) {
                 res.status(200).json(deletionSuccess)
             } else {
@@ -37,13 +40,15 @@ export class Task {
 
     static async update(req: express.Request, res: express.Response) {
         try {
-            const task = await TaskFacade.update(req.body)
+            const task = await TaskFacade.update(req.body, req.params.id)
+            // req.params.id is the id of the Task
             res.status(200).json(task)
         } catch (e) {
             res.status(404).json({ message: e.message})
         }
     }
 
+    /*
     static async create(req: express.Request, res: express.Response) {
         try {
             const task = await TaskFacade.create(req.body, req.params.task_id)
@@ -51,5 +56,5 @@ export class Task {
         } catch (e) {
             res.status(404).json({ message: e.message})
         }
-    }
+    } */
 }
