70d6122b13bf8a9b232c1faf0d02a84415a4bda7
YvesLongchamp <longchampyves84@gmail.com>
2017-11-07 20:34:09 +0100

    wip

--- src/config.ts
+++ src/config.ts
@@ -49,7 +49,7 @@ export const config: Config = {
     },
 
     redis: {
-        host: process.env.REDIS_HOST || '192.168.99.100',
+        host: process.env.REDIS_HOST || '127.0.0.1',
         port: process.env.REDIS_PORT || 6379
     },
 
